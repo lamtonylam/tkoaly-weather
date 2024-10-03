0c9eca5d0449aaa8ef6622155ce5c8c687f52f1b
Tony Lam <tony.lam@iki.fi>
2024-10-03 23:08:18 +0300

    testing

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import icalendar
 import requests
 from flask import Flask
 from datetime import timedelta
+from datetime import datetime
+import pandas as pd
 
 
 weather_url = "https://api.open-meteo.com/v1/forecast?latitude=60.1695&longitude=24.9354&hourly=temperature_2m,precipitation_probability,precipitation&forecast_days=16"
@@ -9,9 +11,16 @@ weather_response = requests.get(weather_url)
 
 weather_data = weather_response.json()
 weather_time_list = weather_data["hourly"]["time"]
+
+# to datetime object
+weather_time_list = [
+    datetime.strptime(time, "%Y-%m-%dT%H:%M") for time in weather_time_list
+]
+
 weather_tempature_list = weather_data["hourly"]["temperature_2m"]
 weather_rain_list = weather_data["hourly"]["precipitation_probability"]
 weather_precipitation_probability = weather_data["hourly"]["precipitation"]
+
 weather_data_zipped = list(
     zip(
         weather_time_list,
@@ -21,58 +30,37 @@ weather_data_zipped = list(
     )
 )
 
-weather_time_list_fixed = []
-
-for i in weather_data_zipped:
-    day = i[0][:10]
-    hour = i[0][11:14]
-    weather_time_list_fixed.append(f"{day} {hour}")
-
-weather_data_zipped_fixed = zip(
-    weather_time_list_fixed,
-    weather_tempature_list,
-    weather_rain_list,
-    weather_precipitation_probability,
-)
 
 calendar_url = "https://ics.tko-aly.fi/"
 
 response = requests.get(calendar_url)
 
-
-# Function to round time to the nearest hour
-def round_time_to_nearest_hour(dt):
-    if dt.minute >= 30:
-        dt = dt + timedelta(hours=1)
-    return dt.replace(minute=0, second=0, microsecond=0)
-
-
 event_dict = {}
 
 calendar = icalendar.Calendar.from_ical(response.content)
+
 for event in calendar.walk("VEVENT"):
     name = str(event.decoded("SUMMARY"), "utf-8")
-    date = str(event.decoded("dtstart"))
     gmt_date = event.decoded("dtstart")
+    pandas_event_date = pd.to_datetime(gmt_date)
+    rounded_event_date = pandas_event_date.floor("H").to_pydatetime()
 
-    for i in weather_data_zipped_fixed:
-        weather_date_hour = i[0][:13]
-        if weather_date_hour == date[:13]:
-            event_dict[name] = [
-                f"{str(gmt_date + timedelta(hours=3))[:-9]} <br> <b>{round(i[1])} °C</b>  <br> sateen todennäköisyys: <b>{i[2]}%</b> <br> sademäärä: <b>{i[3]} mm</b>"
-            ]
-            break
-
-app = Flask(__name__)
+    for i in weather_data_zipped:
+        weather_date = i[0]
+        if weather_date == rounded_event_date:
+            print(name)
 
 
-@app.route("/")
-def hello_world():
-    html = "<h1>Tekis Weather</h1>"
-    for event, weather in event_dict.items():
-        html += f"<b><p>{event}</b>: <br> {weather[0]}</p>"
-    html = (
-        html
-        + "<br><br><br><b>  </b> <br> <a href='https://www.cs.helsinki.fi/u/tonylam/'>Tony Lam</a>"
-    )
-    return html
+# app = Flask(__name__)
+
+
+# @app.route("/")
+# def hello_world():
+#     html = "<h1>Tekis Weather</h1>"
+#     for event, weather in event_dict.items():
+#         html += f"<b><p>{event}</b>: <br> {weather[0]}</p>"
+#     html = (
+#         html
+#         + "<br><br><br><b>  </b> <br> <a href='https://www.cs.helsinki.fi/u/tonylam/'>Tony Lam</a>"
+#     )
+#     return html
